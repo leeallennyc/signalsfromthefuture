3f8cb62d53692dfd74933d27f34887feb907e32a
leekuczewski <lee@mergingfutures.com>
2021-04-13 13:39:36 -0400

    Update Vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 // vue.config.js file to be place in the root of your repository
-
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/signalsfromthefuture/'
-      : '/'
-  }
+  devServer: {
+    disableHostCheck: true
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/signalsfromthefuture/'
+    : '/'
+};
